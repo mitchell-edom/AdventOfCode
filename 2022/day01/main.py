5e2f3e3a7eb5285405cbedf1ca3e0cd9ddaeb317
mitchell-edom <mitchell.edom@woodside.com.au>
2022-12-01 15:04:01 -0800

    day01

---
+++ 2022/day01/main.py
@@ -0,0 +1,22 @@
+import re
+
+
+EMPTY_LINE_REGEX = r"(?:\r?\n){2,}"
+INPUT_TXT = "input.txt"
+
+
+def main():
+
+    with open(INPUT_TXT) as f:
+        results = []
+        elves = re.split(EMPTY_LINE_REGEX, f.read().strip())
+        for elf in elves:
+            results.append(sum([int(i) for i in elf.split('\n')]))
+    
+    print(f"solution 1: {max(results)}")
+    print(f"solution 2: {sum(sorted(results)[-3:])}")
+
+
+if __name__ == "__main__":
+    main()
+
